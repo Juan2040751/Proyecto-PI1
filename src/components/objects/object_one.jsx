0b213a7c96b140d3d6aab2e586a023c479c5776d
unknown <grijalbaalejo@gmail.com>
2023-06-05 22:38:14 -0400

    Objetos 3D 3.1

--- src/components/objects/object_one.jsx
+++ src/components/objects/object_one.jsx
@@ -1,34 +1,88 @@
-import { Html, useGLTF, useScroll } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
+import { Html, useGLTF, useScroll, OrbitControls } from "@react-three/drei";
+import { Canvas, extend, useThree, useFrame } from "@react-three/fiber";
 import gsap from "gsap";
-import React, { useLayoutEffect, useRef } from "react";
-import "../events/events.css";
+import React, { useLayoutEffect, useRef, useState } from "react";
+// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { useSpring, a, config } from "@react-spring/three";
+import "./objects.css";
+
+// extend({ OrbitControls });
+
+// const sizes = {
+//   width: window.innerWidth,
+//   height: window.innerHeight,
+// };
+
+// const Controls = () => {
+//   const orbitRef = useRef();
+//   const { camera, gl } = useThree();
+//   useFrame(() => {
+//     orbitRef.current.update();
+//   });
+
+//   return (
+//     <OrbitControls
+//       autoRotate
+//       maxPolarAngle={Math.PI / 3}
+//       minPolarAngle={Math.PI / 3}
+//       args={[camera, gl.domElement]}
+//       ref={orbitRef}
+//     />
+//   );
+// };
+
+export function texto() {
+  return (
+    <Html>
+      <div>
+        <h1>Objeto Uno</h1>
+      </div>
+    </Html>
+  );
+}
 
 export function ObjectOne(props) {
   const { nodes, materials } = useGLTF("../static/the_sumerian_king_list.glb");
   return (
-    <group {...props} dispose={null}>
-      <group rotation={[-Math.PI / 2, 0, 0]}>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Object_2.geometry}
-          material={materials["The_Sumerian_King_List_250k_faces.1001"]}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Object_3.geometry}
-          material={materials["The_Sumerian_King_List_250k_faces.1001"]}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Object_4.geometry}
-          material={materials["The_Sumerian_King_List_250k_faces.1001"]}
-        />
+    <Canvas
+      flat
+      color="CB4154"
+      shadows
+      camera={{
+        position: [6, 0, 0],
+      }}
+    >
+      <group {...props}>
+        <group position={(0, 1, 1)}>
+          <directionalLight
+            castShadow
+            shadow-normalBias={0.04}
+            position={[0, -3, 3]}
+            intensity={1.5}
+          />
+          <OrbitControls enablePan enableRotate enableZoom reverseOrbit />
+          
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Object_2.geometry}
+            material={materials["The_Sumerian_King_List_250k_faces.1001"]}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Object_3.geometry}
+            material={materials["The_Sumerian_King_List_250k_faces.1001"]}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Object_4.geometry}
+            material={materials["The_Sumerian_King_List_250k_faces.1001"]}
+          />
+        </group>
       </group>
-    </group>
+    </Canvas>
   );
 }
 
