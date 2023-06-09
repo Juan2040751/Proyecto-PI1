a59a9618fd2c428d6654f48f84e89d002086aa6e
Juan2040751 <82058717+Juan2040751@users.noreply.github.com>
2023-06-09 18:08:57 -0500

    Test Spring 2

--- src/app.test.js
+++ src/app.test.js
@@ -13,6 +13,12 @@ import Landing from './components/landing';
 import Register from './registro';
 import { BrowserRouter } from 'react-router-dom';
 import Login from './login';
+import NavbarApp from './components/navbar';
+import { ObjectOne } from './components/objects/object_one';
+import Museum from './components/museum';
+import Gastronomy from './components/Gastronomy';
+import Home from './home';
+
 describe("Pruebas HU Sprint 1", () => {
 
     test('PI1-11', async () => {
@@ -28,23 +34,39 @@ describe("Pruebas HU Sprint 1", () => {
         expect(screen.getByText("Ciudades Sumerias")).toBeInTheDocument();
     })
     test('PI1-19', async () => {
-        const renderer = await ReactThreeTestRenderer.create(
-            <BrowserRouter><Register /></BrowserRouter>
-
-        )
-        const component = renderer.scene.children[0]._fiber
-        expect(component.visible).toBeTruthy()
-
+        render(<BrowserRouter><Register /></BrowserRouter>)
+        expect(screen.getByText("Registro de Usuario")).toBeInTheDocument();
     })
     test('PI1-20', async () => {
+        render(<BrowserRouter><Login /></BrowserRouter>)
+        expect(screen.getByText("Hey, bienvenido 👋")).toBeInTheDocument();
+    })
+
+
+})
+describe("Pruebas HU Sprint 2", () => {
+    test('PI1-8', async () => {
         const renderer = await ReactThreeTestRenderer.create(
-            <BrowserRouter><Login /></BrowserRouter>
-
+            <Museum />
         )
-        const component = renderer.scene.children[0]._fiber
-        expect(component.scale).toEqual({x:1, y:1, z:1})
+        expect(renderer.scene._fiber.receiveShadow).toBeFalsy();
+    })
+    test('PI1-9', async () => {
+        render(<EventOne />)
+        expect(screen.getByText("Ciudades Sumerias")).toBeInTheDocument();
+    })
 
+    test('PI1-12', async () => {
+        const renderer = await ReactThreeTestRenderer.create(
+            <Gastronomy />
+        )
+        expect(renderer.getInstance().visible).toBeTruthy();
+    })
+    test('PI1-14', async () => {
+        const renderer = await ReactThreeTestRenderer.create(
+           <Museum/>
+        )
+        console.log(renderer.getInstance().type)
+        expect(renderer.getInstance().type).toBe('Group')
     })
 })
-
-//describe("Pruebas HU Sprint 2", () => {})
