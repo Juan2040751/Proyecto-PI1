f217ca5af52731532ce3757d92f8bb5a45ec3bc7
Alejandr Grijalba <grijalbaalejo@gmail.com>
2023-06-08 22:32:15 -0400

    Comentarios

--- src/components/museum.jsx
+++ src/components/museum.jsx
@@ -11,6 +11,16 @@ import { useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import "./events/events.css";
 
+/**
+ * Componente Museum
+ *
+ * Este componente representa un museo para mostrar los objetos 3d con la ayuda de modals.
+ * 
+ * Forma parte de la historia de usuario PI1-14: Como usuario, espero que se 
+ * presenten algunos objetos de la cultura sumeria en tercera dimensión, para 
+ * poder admirar el objeto en diferentes ángulos.
+ */
+
 function Museum() {
   const [obj3d, setObj3d] = useState({
     uno: false,
